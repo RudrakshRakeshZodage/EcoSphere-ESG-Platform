a989d413c3fb876bdfc3687f7d01ae49ed146a38
RudrakshRakeshZodage <rudrakshrakeshzodage@gmail.com>
2026-07-12 14:08:03 +0530

    allow all vercel.app subdomains in CORS settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,6 +32,7 @@ origins = [origin.strip() for origin in config.cors_origins.split(",")]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
